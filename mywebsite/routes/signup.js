ca4c2c512ab8304a7fc62754adddae3c904420c6
Yashashvi Dave <yashashvidave@gmail.com>
2021-10-21 21:55:06 -0500

    Add signup setup

--- mywebsite/routes/signup.js
+++ mywebsite/routes/signup.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('signup', { title: 'Sign up to MyWebsite' });
 });
 
+router.post('/', function(req, res, next) {
+  res.render('signup', { signup: true });
+});
+
 module.exports = router;

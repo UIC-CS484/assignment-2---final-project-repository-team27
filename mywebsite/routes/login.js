71949fba45ea6013fb3ee3e0a95aac60ab00682f
Yashashvi Dave <yashashvidave@gmail.com>
2021-10-21 21:44:29 -0500

    Add login setup

--- mywebsite/routes/login.js
+++ mywebsite/routes/login.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'MyWebsite' });
 });
 
+router.post('/', function(req, res, next) {
+    req.app.set('username', req.body.username);
+    // console.log(req.body.username);
+    res.redirect('/user');
+});
+
 module.exports = router;
--- mywebsite/routes/user.js
+++ mywebsite/routes/user.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user', { title: 'MyWebsite' });
+    console.log(req.app.get('username'))
+  res.render('user', { title: 'MyWebsite', username: req.app.get('username') });
 });
 
 module.exports = router;

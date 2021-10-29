d9b1fb5b4b66b085d5f98b3548512c25410e0267
Yashashvi Dave <yashashvidave@gmail.com>
2021-10-29 14:41:01 -0500

    Sign up errors

--- mywebsite/routes/signup.js
+++ mywebsite/routes/signup.js
@@ -18,22 +18,50 @@ router.post('/', async function(req, res, next) {
     const lname = req.body.lname;
     const email = req.body.email;
     const password = req.body.password;
+    var signup_errors = new Map();
 
     if (req.app.get('env') === 'production') {
+        if (fname.length < 1) {
+            signup_errors.set('fname_error', 'First name can not be empty');
+        }
+        if (fname.length > 100) {
+            signup_errors.set('fname_error', 'First name can not be longer than 100 characters');
+        }
+        if (lname.length < 1) {
+            signup_errors.set('lname_error', 'Last name can not be empty');
+        }
+        if (lname.length > 100) {
+            signup_errors.set('lname_error', 'Last name can not be longer than 100 characters');
+        }
+
         if (!emailValidator.validate(email)) {
-          return res.render('signup', { email_error: "Email address is not valid" });
+            signup_errors.set('email_error', "Email address is not valid");
+          // return res.render('signup', { email_error: "Email address is not valid" });
         }
 
         const errors = pwdLib.validatePassword(password, true);
         if (errors.length != 0) {
           // console.log(errors);
-          return res.render('signup', { pwderrors: errors });
+          // return res.render('signup', { pwderrors: errors });
+            signup_errors.set('pwderrors', errors);
         }
         // console.log(errors);
 
         const user = userLib.getUserByEmail(email);
         if (user != null) {
-            return res.render('signup', { error: 'User already exists. Please login.' });
+            signup_errors.set('exists_error', "Email adress is already exists. Please login.");
+            // return res.render('signup', { error: 'User already exists. Please login.' });
+        }
+
+        if (signup_errors.size !== 0) {
+            signup_errors.set('error', 'Please address following errors');
+            signup_errors.set('fname', fname);
+            signup_errors.set('lname', lname);
+            signup_errors.set('email', email);
+            let signup_errors_obj = Array.from(signup_errors).reduce((obj, [key, value]) => (
+              Object.assign(obj, { [key]: value }) 
+            ), {});
+            return res.render('signup', signup_errors_obj);
         }
     }
     // console.log(users);
